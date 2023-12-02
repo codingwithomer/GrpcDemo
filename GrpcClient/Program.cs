73b56b6ef6df0e6dfb0a894ff570b19298904e00
Omer Bilal Can <omerbilalcan@gmail.com>
2023-12-02 22:05:56 +0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest
+            //{
+            //    Name = "Ömer Bilal"
+            //};
+
+            //var channel = GrpcChannel.ForAddress("https://localhost:7077");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:7077");
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var clientRequest = new CustomerLookupModel
+            {
+                UserId = 2
+            };
+
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequest);
+
+            Console.WriteLine($"{ customer.FirstName } { customer.LastName }");
+
+            Console.WriteLine();
+            Console.WriteLine("New Customer List");
+            Console.WriteLine();
+
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while(await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+
+                    Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName}: {currentCustomer.EmailAddress}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,77 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            }
+            else
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(
+            NewCustomerRequest request, 
+            IServerStreamWriter<CustomerModel> responseStream, 
+            ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Ömer Bilal",
+                    LastName = "Can",
+                    EmailAddress = "ocan@luxoft.com",
+                    Age = 37,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Tim",
+                    LastName = "Corey",
+                    EmailAddress = "tim@iamtimcorey.com",
+                    Age = 41,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Bilbo",
+                    LastName = "Baggins",
+                    EmailAddress = "bilbo@middleearth.com",
+                    Age = 117,
+                    IsAlive = false
+                }
+            };
+
+            foreach (var customer in customers)
+            {
+                await responseStream.WriteAsync(customer);
+            }
+        }
+    }
+}
